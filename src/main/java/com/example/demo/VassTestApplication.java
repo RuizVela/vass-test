d2fde3af68457fc70f48e2ee675b954821bc9d0b
Raúl <ruizvela271@gmail.com>
2022-12-11 16:48:16 +0100

    first commit

---
+++ src/main/java/com/example/demo/VassTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VassTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VassTestApplication.class, args);
+	}
+
+}
